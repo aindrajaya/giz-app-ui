3db334164ed85c4ff0ce19db0a5e2835e507427f
pr3b <eb.underwood22@gmail.com>
2023-03-27 12:41:57 +0700

    giz-app

--- src/components/FormBox.jsx
+++ src/components/FormBox.jsx
@@ -1,14 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import styles from "../styles/Main.module.css";
+import ImageResultBox from './ImageResultBox';
 
 const FormBox = () => {
-  const handleCalculation = () => {
-    console.log('calculation');
+  const [loading, setLoading] = useState(false);
+
+  const handleCalculation = (event) => {
+    event.preventDefault()
+    setLoading(true);
+    console.log('image and scale');
   }
 
   return (
     <form className={styles.formBoxContainer}>
+      <h2 className={styles.title}>Input your image</h2>
+      <label>
+        Upload File
+        <br />
+        {/* <input className={styles.label} type="file" name="image" accept="image/*" /> */}
+        <input className={styles.label} type="file" accept="image/*" />
+      </label>
+      <label>
+        Scale
+        <br />
+        <input className={styles.label} type="number" name="scalepixel" placeholder='number in (pixel)' /> : <input className={styles.label} type="number" name="scalemeter" placeholder='number in (m)' />
+      </label>
+      <br />
+      <div className={`${styles.container} ${loading ? styles.loading : ''}`}>
+      {loading ? <p>Loading...</p> : <p>Image Uploaded!</p>}
+    </div>
+
       <div className={styles.horizontalShape}></div>
       <label>
         Input width and length per module (m)
--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -1,19 +1,13 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import FormBox from './FormBox';
 import styles from "../styles/Main.module.css";
-import Image from 'next/image';
-import building from '../../public/assets/building.png'
-import ImageBox from './ImageBox';
 import FormResultBox from './FormResultBox';
 import ImageResultBox from './ImageResultBox';
 
 function MainForm() {
-
       return (
         <div className={styles.main}>
           <div className={styles.ImageForm}>
-            <ImageBox />
             <FormBox />
           </div>
           <div className={styles.resultContainer}>
